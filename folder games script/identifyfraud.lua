6bbdb42920f48f043bbb8bfbff30075ea62f2a0c
airesz17890 <149053428+airesz17890@users.noreply.github.com>
2023-10-30 05:27:58 +0800

    Update identifyfraud.lua

--- folder games script/identifyfraud.lua
+++ folder games script/identifyfraud.lua
@@ -8,7 +8,14 @@ local window = Library:AddWindow(game:GetService("MarketplaceService"):GetProduc
 local tab1 = window:AddTab("Main")
 local tab2 = window:AddTab("Esp")
 tab1:Show()
-local fly = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nicuse/RobloxScripts/main/BypassedFly.lua"))()
+lplayer = game:GetService("Players").LocalPlayer
+lplayer.CharacterAdded:Connect(function(character)
+    flying = false
+end)
+local Mouse = lplayer:GetMouse()
+flying = false
+flying = false
+speedfly = 2
 local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))()
 ESP:Toggle(true)
 ESP.Tracers = false
@@ -81,7 +88,77 @@ tab1:AddSwitch("Noclip", function(v)
     end
 end)
 tab1:AddSwitch("Fly", function(v)
-    fly(v)
+    getgenv().flying = v
+    if getgenv().flying then
+        repeat wait() until lplayer and lplayer.Character and lplayer.Character:FindFirstChild('HumanoidRootPart') and lplayer.Character:FindFirstChild('Humanoid')
+        repeat wait() until Mouse
+        local T = lplayer.Character.HumanoidRootPart
+        local CONTROL = {F = 0, B = 0, L = 0, R = 0}
+        local lCONTROL = {F = 0, B = 0, L = 0, R = 0}
+        local SPEED = speedget
+        local function fly()
+            flying = true
+            local BG = Instance.new('BodyGyro', T)
+            local BV = Instance.new('BodyVelocity', T)
+            BG.P = 9e4
+            BG.maxTorque = Vector3.new(9e9, 9e9, 9e9)
+            BG.cframe = T.CFrame
+            BV.velocity = Vector3.new(0, 0.1, 0)
+            BV.maxForce = Vector3.new(9e9, 9e9, 9e9)
+            spawn(function()
+                repeat wait()
+                    lplayer.Character.Humanoid.PlatformStand = true
+                    if CONTROL.L + CONTROL.R ~= 0 or CONTROL.F + CONTROL.B ~= 0 then
+                        SPEED = 50
+                    elseif not (CONTROL.L + CONTROL.R ~= 0 or CONTROL.F + CONTROL.B ~= 0) and SPEED ~= 0 then
+                        SPEED = 0
+                    end
+                    if (CONTROL.L + CONTROL.R) ~= 0 or (CONTROL.F + CONTROL.B) ~= 0 then
+                        BV.velocity = ((workspace.CurrentCamera.CoordinateFrame.lookVector * (CONTROL.F + CONTROL.B)) + ((workspace.CurrentCamera.CoordinateFrame * CFrame.new(CONTROL.L + CONTROL.R, (CONTROL.F + CONTROL.B) * 0.2, 0).p) - workspace.CurrentCamera.CoordinateFrame.p)) * SPEED
+                        lCONTROL = {F = CONTROL.F, B = CONTROL.B, L = CONTROL.L, R = CONTROL.R}
+                    elseif (CONTROL.L + CONTROL.R) == 0 and (CONTROL.F + CONTROL.B) == 0 and SPEED ~= 0 then
+                        BV.velocity = ((workspace.CurrentCamera.CoordinateFrame.lookVector * (lCONTROL.F + lCONTROL.B)) + ((workspace.CurrentCamera.CoordinateFrame * CFrame.new(lCONTROL.L + lCONTROL.R, (lCONTROL.F + lCONTROL.B) * 0.2, 0).p) - workspace.CurrentCamera.CoordinateFrame.p)) * SPEED
+                    else
+                        BV.velocity = Vector3.new(0, 0.1, 0)
+                    end
+                    BG.cframe = workspace.CurrentCamera.CoordinateFrame
+                until not flying
+                CONTROL = {F = 0, B = 0, L = 0, R = 0}
+                lCONTROL = {F = 0, B = 0, L = 0, R = 0}
+                SPEED = 0
+                BG:destroy()
+                BV:destroy()
+                lplayer.Character.Humanoid.PlatformStand = false
+            end)
+        end
+        Mouse.KeyDown:connect(function(KEY)
+            if KEY:lower() == 'w' then
+                CONTROL.F = speedfly
+            elseif KEY:lower() == 's' then
+                CONTROL.B = -speedfly
+            elseif KEY:lower() == 'a' then
+                CONTROL.L = -speedfly
+            elseif KEY:lower() == 'd' then
+                CONTROL.R = speedfly
+            end
+        end)
+        Mouse.KeyUp:connect(function(KEY)
+            if KEY:lower() == 'w' then
+                CONTROL.F = 0
+            elseif KEY:lower() == 's' then
+                CONTROL.B = 0
+            elseif KEY:lower() == 'a' then
+                CONTROL.L = 0
+            elseif KEY:lower() == 'd' then
+                CONTROL.R = 0
+            end
+        end)
+        fly()
+    end
+    if not getgenv().flying then
+        flying = false
+        lplayer.Character.Humanoid.PlatformStand = false
+    end
 end)
 tab1:AddSwitch("Inf Jump", function(v)
     getgenv().infjump = v
