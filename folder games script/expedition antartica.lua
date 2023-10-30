d5b7bb0ef518970f3ab56631eeae3ad5a6468e62
airesz17890 <149053428+airesz17890@users.noreply.github.com>
2023-10-31 00:32:36 +0800

    Update expedition antartica.lua

--- folder games script/expedition antartica.lua
+++ folder games script/expedition antartica.lua
@@ -18,7 +18,14 @@ local window = Library:AddWindow(game:GetService("MarketplaceService"):GetProduc
     min_size = Vector2.new(250, 346),
     can_resize = true,
 })
-local fly = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nicuse/RobloxScripts/main/BypassedFly.lua"))()
+lplayer = game:GetService("Players").LocalPlayer
+lplayer.CharacterAdded:Connect(function(character)
+    flying = false
+end)
+local Mouse = lplayer:GetMouse()
+flying = false
+flying = false
+speedfly = 2
 local tab1 = window:AddTab("Main")
 local tab2 = window:AddTab("Esp")
 local tab3 = window:AddTab("Misc")
@@ -116,9 +123,9 @@ folder2:AddSwitch("Inf Water", function(v)
     end)
 end)
 folder2:AddSwitch("Inf Flare", function(v)
-    getgenv().flare = v
-    game:GetService("Players").carpor178.PlayerGui["Flare_Counter"].Frame["Flare_launch"].Activated:Connect(function()
-        if getgenv().flare then
+    getgenv().inf = true
+    game:GetService("Players").LocalPlayer.PlayerGui["Flare_Counter"].Frame.Direction:GetPropertyChangedSignal("Text"):Connect(function()
+        if getgenv().inf then
             game:GetService("Players").LocalPlayer.PlayerGui["Flare_Counter"].Frame.Direction.Text = "3 / 3"
         end
     end)
@@ -181,7 +188,77 @@ spawn(function()
     end
 end)
 tab3:AddSwitch("Fly", function(v)
-    fly(v)
+    getgenv().flying = v
+    if getgenv().flying then
+        repeat wait() until lplayer and lplayer.Character and lplayer.Character:FindFirstChild('HumanoidRootPart') and lplayer.Character:FindFirstChild('Humanoid')
+        repeat wait() until Mouse
+        local T = lplayer.Character.HumanoidRootPart
+        local CONTROL = {F = 0, B = 0, L = 0, R = 0}
+        local lCONTROL = {F = 0, B = 0, L = 0, R = 0}
+        local SPEED = speedget
+        local function fly()
+            flying = true
+            local BG = Instance.new('BodyGyro', T)
+            local BV = Instance.new('BodyVelocity', T)
+            BG.P = 9e4
+            BG.maxTorque = Vector3.new(9e9, 9e9, 9e9)
+            BG.cframe = T.CFrame
+            BV.velocity = Vector3.new(0, 0.1, 0)
+            BV.maxForce = Vector3.new(9e9, 9e9, 9e9)
+            spawn(function()
+                repeat wait()
+                    lplayer.Character.Humanoid.PlatformStand = true
+                    if CONTROL.L + CONTROL.R ~= 0 or CONTROL.F + CONTROL.B ~= 0 then
+                        SPEED = 50
+                    elseif not (CONTROL.L + CONTROL.R ~= 0 or CONTROL.F + CONTROL.B ~= 0) and SPEED ~= 0 then
+                        SPEED = 0
+                    end
+                    if (CONTROL.L + CONTROL.R) ~= 0 or (CONTROL.F + CONTROL.B) ~= 0 then
+                        BV.velocity = ((workspace.CurrentCamera.CoordinateFrame.lookVector * (CONTROL.F + CONTROL.B)) + ((workspace.CurrentCamera.CoordinateFrame * CFrame.new(CONTROL.L + CONTROL.R, (CONTROL.F + CONTROL.B) * 0.2, 0).p) - workspace.CurrentCamera.CoordinateFrame.p)) * SPEED
+                        lCONTROL = {F = CONTROL.F, B = CONTROL.B, L = CONTROL.L, R = CONTROL.R}
+                    elseif (CONTROL.L + CONTROL.R) == 0 and (CONTROL.F + CONTROL.B) == 0 and SPEED ~= 0 then
+                        BV.velocity = ((workspace.CurrentCamera.CoordinateFrame.lookVector * (lCONTROL.F + lCONTROL.B)) + ((workspace.CurrentCamera.CoordinateFrame * CFrame.new(lCONTROL.L + lCONTROL.R, (lCONTROL.F + lCONTROL.B) * 0.2, 0).p) - workspace.CurrentCamera.CoordinateFrame.p)) * SPEED
+                    else
+                        BV.velocity = Vector3.new(0, 0.1, 0)
+                    end
+                    BG.cframe = workspace.CurrentCamera.CoordinateFrame
+                until not flying
+                CONTROL = {F = 0, B = 0, L = 0, R = 0}
+                lCONTROL = {F = 0, B = 0, L = 0, R = 0}
+                SPEED = 0
+                BG:destroy()
+                BV:destroy()
+                lplayer.Character.Humanoid.PlatformStand = false
+            end)
+        end
+        Mouse.KeyDown:connect(function(KEY)
+            if KEY:lower() == 'w' then
+                CONTROL.F = speedfly
+            elseif KEY:lower() == 's' then
+                CONTROL.B = -speedfly
+            elseif KEY:lower() == 'a' then
+                CONTROL.L = -speedfly
+            elseif KEY:lower() == 'd' then
+                CONTROL.R = speedfly
+            end
+        end)
+        Mouse.KeyUp:connect(function(KEY)
+            if KEY:lower() == 'w' then
+                CONTROL.F = 0
+            elseif KEY:lower() == 's' then
+                CONTROL.B = 0
+            elseif KEY:lower() == 'a' then
+                CONTROL.L = 0
+            elseif KEY:lower() == 'd' then
+                CONTROL.R = 0
+            end
+        end)
+        fly()
+    end
+    if not getgenv().flying then
+        flying = false
+        lplayer.Character.Humanoid.PlatformStand = false
+    end
 end)
 tab3:AddSwitch("Noclip", function(no)
     getgenv().noclip = no
@@ -212,7 +289,7 @@ tab3:AddButton("Less Lag", function()
         mx.MaxItems = mx3 * .9999999999
     end
     wait()
-    setfpscap(1000)
+    setfpscap(3000)
 end)
 tab3:AddButton("Fullbright", function()
     loadstring(game:HttpGet('https://pastebin.com/raw/06iG6YkU'))()
